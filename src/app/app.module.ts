dfcedd09cd35fb782423383933ed409fa9f59572
Bryan Luis <baluisto@correo.udistrital.edu.co>
2021-04-05 11:08:22 -0500

    Creada esctructura basica

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PostComponent } from './components/post/post.component';
+import { CommentsComponent } from './components/comments/comments.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PostComponent,
+    CommentsComponent
   ],
   imports: [
     BrowserModule,
